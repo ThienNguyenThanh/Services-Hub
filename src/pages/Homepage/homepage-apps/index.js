0d0d658d0d107be71b2fb3e1d053f8d47ddf55c3
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2022-07-07 15:29:12 +0700

    navigate to other page

---
+++ src/pages/Homepage/homepage-apps/index.js
@@ -0,0 +1,34 @@
+
+
+Component({
+	data: {
+    categoryList: ['Phổ biến', 'Lịch sử', 'Yêu thích'],
+    category: '',
+    apps: {
+      url: '',
+    },
+    action: 'Xem thêm',
+    onTapActionButton: () => {},
+    navigateTo: () => {},
+  },
+  methods: {
+    didMount() {
+      this.setData({
+        category: this.data.categoryList.pop(),
+        action: this.data.action,
+      })
+    },
+    _onTapActionButton() {
+      if (this.data.category == 'Phổ biến') {
+        my.navigateTo({url: '../apps-pages/popular-page/index'})
+      }
+      if (this.data.category == 'Lịch sử') {
+        my.navigateTo({url: '../apps-pages/history-page/index'})
+      }
+      if (this.data.category == 'Yêu thích') {
+        my.navigateTo({url: '../apps-pages/favorite-page/index'})
+      }
+    },
+  
+  }, 
+});

ab587fd7d3a879b2bfd0ec66486bb5e095b7aa38
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2022-07-09 18:31:46 +0700

    Khoi 9.7.Final

--- src/pages/resultPage/app-result/index.js
+++ src/pages/resultPage/app-result/index.js
@@ -2,46 +2,55 @@ import file from "./index.json"
 
 Component({
   props: {
-    categoryList: [],
-    category: '',
-    miniApps: [
-      
-    ],
-    miniApp: {
-    }
-    // titleList: [],
-    // title: '',
-    // descriptionList: [],
-    // description: ''
   },
-  didMount() {
-    // this.props = file.data
   
+  didMount() {
     let values = Object.values(file.data)
-    const miniData = new Map()
-    values.forEach(element => {
-      miniData.set(element.category, element.miniApps)
+    this.props = new Map()
+    values.forEach(item => {
+      this.props.set(item.category, item.miniApps)
+      console.log(item.category)
+      this.setData({
+        props: item,
+        category: item.category,
+        miniApps: item.miniApps
+      })
     });
-    for (let i = 0; i < values.length; i ++){
-      this.props.categoryList.push(values[i].category)
-    }
-    console.log(this.props.categoryList)
-    for (let i = 0; i < this.props.categoryList.length; i++){
-      let temp = this.props.categoryList[i]
-      this.props.category = temp
-      let appTemp;
-      if (miniData.has(temp)){
-        appTemp = miniData.get(temp)
-        this.props.miniApps.push(miniData.get(temp))  
-        for (let j = 0; j < appTemp.length; j++){
-          this.props.miniApp = appTemp[j]
-        }
-      }
-    }
-    console.log(this.props.miniApps)
-    // console.log(this.props.category)
-    console.log(this.props.miniApp.title)
-    this.setData({props: this.props})
+    this.props.forEach(category => {
+      // console.log(category)
+      category.forEach(miniApp => {
+        // console.log(miniApp)
+        this.setData({
+          title: miniApp.title,
+          description: miniApp.description
+        })
+      })
+      // this.setData({
+      //   category.forEach(mini)
+      // })
+    })
+   
+    // for (let i = 0; i < values.length; i ++){
+    //   this.props.categoryList.push(values[i].category)
+    // }
+    // console.log(this.props.categoryList)
+    // for (let i = 0; i < this.props.categoryList.length; i++){
+    //   let temp = this.props.categoryList[i]
+    //   this.props.category = temp
+    //   let appTemp;
+    //   if (miniData.has(temp)){
+    //     appTemp = miniData.get(temp)
+    //     this.props.miniApps.push(miniData.get(temp))  
+    //     for (let j = 0; j < appTemp.length; j++){
+    //       this.props.miniApp = appTemp[j]
+    //     }
+    //   }
+    // }
+    // console.log(this.props.miniApps)
+    // // console.log(this.props.category)
+    // console.log(this.props.miniApp.title)
+    
+    
   }
 })
     
